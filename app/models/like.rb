3ac78f3d1e7eea5d0c4e21b281783a2f92a5fd3d
veliancreate <julian_veling@hotmail.com>
2015-04-06 23:01:58 +0100

    added likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
-class Comment < ActiveRecord::Base
+class Like < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Post, type: :model do
-  it { is_expected.to have_many :comments }
+  it { is_expected.to have_many :comments } 
 end
