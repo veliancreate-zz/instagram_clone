919e51a7c5a26b9c207c287f5409f4c8dfc8a0c2
Julian Veling <julian_veling@hotmail.com>
2015-06-03 23:56:48 +0100

    css

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
   $('.likes-link').on('click', function(event){
     event.preventDefault();
 
-    var likeCount = $(this).siblings('.likes_count');
+    var likeCount = $('.likes_count');
+
+    function pluralize_like(number){
+      if(number > 1){ return 'likes' }
+      return 'like'
+    }
 
     $.post(this.href, function(response){
-      likeCount.text(response.new_like_count);
+      likeCount.text(response.new_like_count + " " + pluralize_like(response.new_like_count));
       $('.likes_message').text(response.like_message)
     });
   });
